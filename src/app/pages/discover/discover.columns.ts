e06f350dd3481839bfc777dca487075ee06d8126
AncaStefaniaa <anca.ilicea@tremend.com>
2021-05-14 21:57:36 +0300

    test

--- src/app/pages/discover/discover.columns.ts
+++ src/app/pages/discover/discover.columns.ts
@@ -29,23 +29,8 @@ export const architecturalStyles: any[] = [
       "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Craftsmanhouse.jpg/330px-Craftsmanhouse.jpg",
   },
   {
-    name: "American Foursquare",
-    id: 2,
-    about:
-      "The American Foursquare or American Four Square is an American house style popular from the mid-1890s to the late 1930s. A reaction to the ornate and mass-produced elements of the Victorian and other Revival styles popular throughout the last half of the 19th century, the American Foursquare was plain, often incorporating handcrafted 'honest' woodwork (unless purchased from a mail-order catalog). This style incorporates elements of the Prairie School and the Craftsman styles. It is also sometimes called Transitional Period.",
-    origins: ["United States", "mid-1890s to the late 1930s"],
-    characteristics: [
-      "elements of the Victorian",
-      "boxy design",
-      "wide stairs",
-    ],
-    wikipedia: "https://en.wikipedia.org/wiki/American_Foursquare",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/831Richland.jpg/450px-831Richland.jpg",
-  },
-  {
     name: "Ancient Egyptian",
-    id: 3,
+    id: 2,
     about:
       "Ancient Egypt was a civilization of ancient North Africa, concentrated along the lower reaches of the Nile River, situated in the place that is now the country Egypt. Ancient Egyptian civilization followed prehistoric Egypt and coalesced around 3100 BC (according to conventional Egyptian chronology)[1] with the political unification of Upper and Lower Egypt under Menes (often identified with Narmer).[2] The history of ancient Egypt occurred as a series of stable kingdoms, separated by periods of relative instability known as Intermediate Periods: the Old Kingdom of the Early Bronze Age, the Middle Kingdom of the Middle Bronze Age and the New Kingdom of the Late Bronze Age.",
     origins: [" North Africa", "3100 BC"],
@@ -56,7 +41,7 @@ export const architecturalStyles: any[] = [
   },
   {
     name: "Art Deco",
-    id: 4,
+    id: 3,
     about:
       "Art Deco, sometimes referred to as Deco, is a style of visual arts, architecture and design that first appeared in France just before World War I. Art Deco influenced the design of buildings, furniture, jewelry, fashion, cars, movie theatres, trains, ocean liners, and everyday objects such as radios and vacuum cleaners.It took its name, short for Arts Décoratifs, from the Exposition internationale des arts décoratifs et industriels modernes (International Exhibition of Modern Decorative and Industrial Arts) held in Paris in 1925. It combined modern styles with fine craftsmanship and rich materials. During its heyday, Art Deco represented luxury, glamour, exuberance, and faith in social and technological progress. From its outset, Art Deco was influenced by the bold geometric forms of Cubism and the Vienna Secession; the bright colors of Fauvism and of the Ballets Russes. The Chrysler Building and other skyscrapers of New York City built during the 1920s and 1930s are monuments of the Art Deco style.",
     origins: ["France just before World War I"],
@@ -70,19 +55,8 @@ export const architecturalStyles: any[] = [
       "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/CMC-Union_Terminal.jpg/675px-CMC-Union_Terminal.jpg",
   },
   {
-    name: "Art Nouveau",
-    id: 5,
-    about:
-      "Art Nouveau, ornamental style of art that flourished between about 1890 and 1910 throughout Europe and the United States. Art Nouveau is characterized by its use of a long, sinuous, organic line and was employed most often in architecture, interior design, jewelry and glass design, posters, and illustration. It was a deliberate attempt to create a new style, free of the imitative historicism that dominated much of 19th-century art and design. About this time the term Art Nouveau was coined, in Belgium by the periodical L’Art Moderne to describe the work of the artist group Les Vingt and in Paris by S. Bing, who named his gallery L’Art Nouveau. The style was called Jugendstil in Germany, Sezessionstil in Austria, Stile Floreale (or Stile Liberty) in Italy, and Modernismo (or Modernista) in Spain.",
-    origins: ["about 1890 and 1910", "Europe", "United States"],
-    characteristics: ["use of a long, sinuous, organic line"],
-    wikipedia: "https://en.wikipedia.org/wiki/Art_Nouveau",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/BeursVanBerlage.jpg/600px-BeursVanBerlage.jpg",
-  },
-  {
     name: "Baroque",
-    id: 6,
+    id: 4,
     about:
       "Baroque architecture is a style that emerged in Italy in the late-16th century. It was a more theatrical version of Renaissance architecture, with dramatic lighting and colour, illusory effects such as trompe l’oeil, and designs that played games with architectural features, sometimes leaving them incomplete. Its buildings typically include central towers, domes, portico or other central projections in the main façade. As Baroque architecture coincided with European colonialism, it can be seen throughout much of the world; and in some regions, notably Germany and colonial South America, it lasted until the 18th century.",
     origins: ["early 17th century", "spread across Europe"],
@@ -95,33 +69,8 @@ export const architecturalStyles: any[] = [
       "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Palais_Luxembourg_Sunset_Edit.JPG/675px-Palais_Luxembourg_Sunset_Edit.JPG",
   },
   {
-    name: "Bauhaus",
-    id: 7,
-    about:
-      "The Bauhaus was founded by architect Walter Gropius in Weimar. It was grounded in the idea of creating a Gesamtkunstwerk ('comprehensive artwork') in which all the arts would eventually be brought together. The Bauhaus style later became one of the most influential currents in modern design, modernist architecture and art, design, and architectural education.[2] The Bauhaus movement had a profound influence upon subsequent developments in art, architecture, graphic design, interior design, industrial design, and typography. Staff at the Bauhaus included prominent artists such as Paul Klee, Wassily Kandinsky, and László Moholy-Nagy at various points.",
-    origins: ["from 1919 to 1933", "Germany"],
-    characteristics: [
-      "combined crafts and the fine arts",
-      "comprehensive artwork",
-    ],
-    wikipedia: "https://en.wikipedia.org/wiki/Bauhaus",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/6265_Dessau.JPG/375px-6265_Dessau.JPG",
-  },
-  {
-    name: "Beaux-Arts",
-    id: 8,
-    about:
-      "Beaux-Arts architecture was the academic architectural style taught at the École des Beaux-Arts in Paris, particularly from the 1830s to the end of the 19th century. It drew upon the principles of French neoclassicism, but also incorporated Gothic and Renaissance elements, and used modern materials, such as iron and glass. It was an important style in France until the end of the 19th century. It also had a strong influence on architecture in the United States because of the many prominent American architects who studied at the École des Beaux-Arts, including Henry Hobson Richardson, John Galen Howard, Daniel Burnham, and Louis Sullivan.",
-    origins: ["from the 1830s to the end of the 19th century.", "Paris"],
-    characteristics: ["Modern materials", "Classical details", "Flat roof"],
-    wikipedia: "https://en.wikipedia.org/wiki/Beaux-Arts_architecture",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Paris_6_-_ENSBA_01.jpg/450px-Paris_6_-_ENSBA_01.jpg",
-  },
-  {
     name: "Byzantine",
-    id: 9,
+    id: 5,
     about:
       "The Byzantine era is usually dated from 330 AD, when Constantine the Great moved the Roman capital to Byzantium, which became Constantinople, until the fall of the Byzantine Empire in 1453. However, there was initially no hard line between the Byzantine and Roman empires, and early Byzantine architecture is stylistically and structurally indistinguishable from earlier Roman architecture. This terminology was introduced by modern historians to designate the medieval Roman Empire as it evolved as a distinct artistic and cultural entity centered on the new capital of Constantinople (modern-day Istanbul) rather than the city of Rome and its environs.",
     origins: ["330 AD", "Constantinople"],
@@ -131,35 +80,8 @@ export const architecturalStyles: any[] = [
       "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Fethiye_Museum_9625.jpg/420px-Fethiye_Museum_9625.jpg",
   },
   {
-    name: "Chicago school",
-    id: 10,
-    about:
-      "Chicago's architecture is famous throughout the world and one style is referred to as the Chicago School. Much of its early work is also known as Commercial style.[1] In the history of architecture, the first Chicago School was a school of architects active in Chicago at the turn of the 20th century. They were among the first to promote the new technologies of steel-frame construction in commercial buildings, and developed a spatial aesthetic which co-evolved with, and then came to influence, parallel developments in European Modernism. A 'Second Chicago School' with a modernist aesthetic emerged in the 1940s through 1970s, which pioneered new building technologies and structural systems, such as the tube-frame structure.",
-    origins: ["20th century", "Chicago"],
-    characteristics: [
-      "bundled tube structural",
-      "the world's tallest buildings",
-    ],
-    wikipedia: "https://en.wikipedia.org/wiki/Chicago_school_(architecture)",
-    image:
-      "https://cdn.skyrisecities.com/sites/default/files/images/articles/2016/05/20957/20957-71905.jpg",
-  },
-
-  {
-    name: "Colonial",
-    id: 11,
-    about:
-      "Colonial architecture is an architectural style from a mother country that has been incorporated into the buildings of settlements or colonies in distant locations. Colonists frequently built settlements that synthesized the architecture of their countries of origin with the design characteristics of their new lands, creating hybrid designs.",
-    origins: ["Dutch Colonial", "French Colonial", "American colonial"],
-    characteristics: ["Dutch Colonial", "French Colonial", "American colonial"],
-    wikipedia:
-      "https://en.wikipedia.org/wiki/Colonial_architecture#:~:text=Colonial%20architecture%20is%20an%20architectural,or%20colonies%20in%20distant%20locations.",
-    image:
-      "https://www.nomadepicureans.com/wp-content/uploads/2018/11/colonial-architecture.jpg",
-  },
-  {
     name: "Deconstructivism",
-    id: 12,
+    id: 6,
     about:
       "Deconstructivism is, in fact, not a new architecture style, nor is it an avant-garde movement against architecture or society. It does not follow “rules” or acquire specific aesthetics, nor is it a rebellion against a social dilemma. It is the unleashing of infinite possibilities of playing around with forms and volumes.",
     origins: ["1980s", "New York"],
@@ -173,38 +95,38 @@ export const architecturalStyles: any[] = [
       "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Image-Disney_Concert_Hall_by_Carol_Highsmith_edit-2.jpg/1151px-Image-Disney_Concert_Hall_by_Carol_Highsmith_edit-2.jpg",
   },
   {
-    name: "Edwardian",
-    id: 13,
-    about:
-      "The characteristic features of the Edwardian Baroque style were drawn from two main sources: the architecture of France during the 18th century and that of Sir Christopher Wren in England during the 17th—part of the English Baroque (for this reason Edwardian Baroque is sometimes referred to as 'Wrenaissance'). Sir Edwin Lutyens was a major exponent, designing many commercial buildings in what he termed 'the Grand Style' during the later 1910s and 1920s. This period of British architectural history is considered a particularly retrospective one, since it is contemporary with Art Nouveau.",
-    origins: ["1901–1910", "United Kingdom"],
-    characteristics: ["lighter colours were used", "less complex patterns"],
-    wikipedia: "https://en.wikipedia.org/wiki/Edwardian_architecture",
-    image:
-      "https://cdn.shopify.com/s/files/1/0290/1369/articles/DSC_0281.jpg_5c93b8cb-ded7-4fd9-b53b-5866dccab393_1400x.jpg?v=1591285455",
-  },
-  {
     name: "Georgian",
-    id: 14,
+    id: 7,
     about:
       "Georgian architecture is the name given in most English-speaking countries to the set of architectural styles current between 1714 and 1830. It is named after the first four British monarchs of the House of Hanover—George I, George II, George III, and George IV—who reigned in continuous succession from August 1714 to June 1830. The style was revived in the late 19th century in the United States as Colonial Revival architecture and in the early 20th century in Great Britain as Neo-Georgian architecture; in both it is also called Georgian Revival architecture. In the United States the term 'Georgian' is generally used to describe all buildings from the period, regardless of style; in Britain it is generally restricted to buildings that are 'architectural in intention', and have stylistic characteristics that are typical of the period, though that covers a wide range.",
     origins: ["between 1714 and 1830", "Great Britain"],
-  },
-  { name: "Gothic", id: 15 },
-  { name: "Greek Revival", id: 16 },
-  { name: "International style", id: 17 },
-  { name: "Novelty", id: 18 },
-  { name: "Palladian", id: 19 },
-  { name: "Postmodern", id: 20 },
-  {
-    name: "Queen Anne",
-    id: 21,
+    characteristics: [
+      "marked by symmetry",
+      "proportion based on the classical architecture ",
+    ],
+    wikipedia: "https://en.wikipedia.org/wiki/Georgian_architecture",
     image:
-      "https://www.thoughtco.com/thmb/UWQq1xhkEKjD0ixd5_dFxTPmVqE=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/architecture-queenanne-Pollock-Capps-House-564106841-5abdaea2c67335003757ef89.jpg",
-    about:
-      "The Queen Anne style in British architecture refers to either the English Baroque architectural style that developed around the time of Queen Anne (who reigned from 1702 to 1714) or a revived form that became popular during the last quarter of the 19th century and the early decades of the 20th century (which is known as Queen Anne Revival).In other English-speaking parts of the world, Queen Anne style refers to entirely different styles.",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Massachusetts_Hall%2C_Harvard_University.JPG/330px-Massachusetts_Hall%2C_Harvard_University.JPG",
   },
-  { name: "Romanesque", id: 22 },
-  { name: "Russian Revival", id: 23 },
-  { name: "Tudor Revival", id: 24 },
+  {
+    name: "Gothic",
+    id: 8,
+    about:
+      "Gothic architecture (or pointed architecture) is an architectural style that was particularly popular in Europe from the late 12th century to the 16th century, during the High and Late Middle Ages, surviving into the 17th and 18th centuries in some areas. It evolved from Romanesque architecture and was succeeded by Renaissance architecture. It originated in the Île-de-France region of northern France as a development of Norman architecture. The style at the time was sometimes known as opus Francigenum (lit. French work); the term Gothic was first applied contemptuously during the later Renaissance, by those ambitious to revive the Grecian orders of architecture.",
+    origins: ["Europe", "late 12th century to the 16th century"],
+    characteristics: [
+      "architecture of many castles",
+      "large stained glass windows",
+    ],
+    wikipedia: "https://en.wikipedia.org/wiki/Gothic_architecture",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Prag%2C_Prager_Burg%2C_Veitsdom_--_2019_--_6662.jpg/330px-Prag%2C_Prager_Burg%2C_Veitsdom_--_2019_--_6662.jpg",
+  },
+  { name: "Neoclasical", id: 9 },
+  { name: "Novelty", id: 10 },
+  { name: "Postmodern", id: 11 },
+  { name: "Romanesque", id: 12 },
+  { name: "Russian Revival", id: 13 },
+  { name: "Tudor Revival", id: 14 },
+  { name: "No architecture", id: 15 },
 ];
